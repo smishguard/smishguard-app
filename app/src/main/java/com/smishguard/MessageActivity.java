f60113472311c7954d59ad21005a4acea2d19e79
XxDaniel-AlzatexX <daniel-alzatem@javeriana.edu.co>
2024-09-01 19:55:11 -0500

    Creación del proyecto, Clases y XML Base

---
+++ app/src/main/java/com/smishguard/MessageActivity.java
@@ -0,0 +1,12 @@
+package com.smishguard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
