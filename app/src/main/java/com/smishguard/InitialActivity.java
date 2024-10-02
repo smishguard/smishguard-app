6d4944ee800e32041a163e4974e07501db3ff5a4
XxDaniel-AlzatexX <daniel-alzatem@javeriana.edu.co>
2024-10-01 23:05:01 -0500

    Implementación Registro, Logeo y Conexión de Pantallas Disponibles

--- app/src/main/java/com/smishguard/InitialActivity.java
+++ app/src/main/java/com/smishguard/InitialActivity.java
@@ -1,12 +1,35 @@
 package com.smishguard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.FirebaseApp;
+import com.smishguard.databinding.ActivityInitialBinding;
 
 public class InitialActivity extends AppCompatActivity {
+
+    private ActivityInitialBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityInitialBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        FirebaseApp.initializeApp(this);
+
+        binding.btnGoLoginIni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(InitialActivity.this, LoginActivity.class));
+            }
+        });
+
+        binding.btnGoRegisterIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(InitialActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/smishguard/LoginActivity.java
+++ app/src/main/java/com/smishguard/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.smishguard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.smishguard.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        FirebaseApp.initializeApp(this);
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.setTextMailL.getText().toString().trim();
+                String password = binding.setTextPasswordL.getText().toString().trim();
+
+                if(email.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Por favor ingresa un correo válido", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Por favor ingresa una contraseña válida", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Error en el inicio de sesión: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
+
+        binding.btnGoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
